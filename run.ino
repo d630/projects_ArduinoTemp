dd5aacf6fc0b6d6e678c2f066dd9be833527de19
aramschubert <32179925+aramschubert@users.noreply.github.com>
2017-12-11 14:03:27 +0100

    update

--- run.ino
+++ run.ino
@@ -1,7 +1,6 @@
-#include <Adafruit_Sensor.h>
 #include <math.h>
-#include <DHT.h>
-#include <SevSeg.h>
+#include "DHT.h"
+#include "SevSeg.h"
 
 #define DEBUG 0
 
@@ -10,13 +9,13 @@
 #define DHTPIN 15
 #define DHTTYPE DHT11
 
-#define HUMI_BLUE 70
-#define HUMI_GREEN 40
+#define HUMI_BLUE 60
+#define HUMI_GREEN 50
 #define LED_BLUE 17
 #define LED_GREEN 19
 #define LED_RED 18
-#define TEMPE_GREEN 18
-#define TEMPE_RED 25
+#define TEMPE_GREEN 20
+#define TEMPE_RED 27
 
 #define PIN_BUTTON 14
 
@@ -28,56 +27,62 @@ byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
 DHT dht(DHTPIN, DHTTYPE);
 char humi;
 char tempe;
-char buf[14];
+char buf[6];
+char senState;
 
 void setup() {
-    sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins,0,1);
-    sevseg.setBrightness(SEV_SEG_BRIGHTNESS);
+  sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins, 1, 1);
+  sevseg.setBrightness(SEV_SEG_BRIGHTNESS);
 
-    dht.begin();
+  dht.begin();
 
-    pinMode(PIN_BUTTON, INPUT);
+  pinMode(PIN_BUTTON, INPUT);
 
-    pinMode(LED_RED, OUTPUT);
-    pinMode(LED_BLUE, OUTPUT);
-    pinMode(LED_GREEN, OUTPUT);
-
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_RED, LOW);
-    digitalWrite(LED_BLUE, LOW);
-    digitalWrite(LED_GREEN, LOW);
+  if (digitalRead(PIN_BUTTON))
+    senState ^= 1;
 
-    if (digitalRead(PIN_BUTTON)) {
-        humi = roundf(dht.readHumidity());
-
-        if (humi >= HUMI_BLUE) {
-            digitalWrite(LED_BLUE, HIGH);
-        } else if (humi >= HUMI_GREEN) {
-            digitalWrite(LED_GREEN, HIGH);
-        } else {
-            digitalWrite(LED_RED, HIGH);
-        }
-
-        snprintf(buf, sizeof(buf), "%d %s", humi, "H");
+  if (senState) {
+    humi = roundf(dht.readHumidity());
+    snprintf(buf, sizeof(buf), "%d %s", humi, "H");
+    if (humi >= HUMI_BLUE) {
+      digitalWrite(LED_BLUE, HIGH);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, LOW);
+    } else if (humi >= HUMI_GREEN) {
+      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_BLUE, LOW);
     } else {
-        tempe = roundf(dht.readTemperature());
-
-        if (tempe >= TEMPE_RED) {
-            digitalWrite(LED_RED, HIGH);
-        } else if (tempe >= TEMPE_GREEN) {
-            digitalWrite(LED_GREEN, HIGH);
-        } else {
-            digitalWrite(LED_BLUE, HIGH);
-        }
-
-        snprintf(buf, sizeof(buf), "%d %s", tempe, "C");
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, LOW);
     }
-    
-    sevseg.setChars(buf);
-
-    sevseg.refreshDisplay();
+  } else {
+    tempe = roundf(dht.readTemperature());
+    snprintf(buf, sizeof(buf), "%d %s", tempe, "C");
+    if (tempe >= TEMPE_RED) {
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, LOW);
+    } else if (tempe >= TEMPE_GREEN) {
+      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_BLUE, LOW);
+    } else {
+      digitalWrite(LED_BLUE, HIGH);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, LOW);
+    }
+  }
+  
+  sevseg.setChars(buf);
+  sevseg.refreshDisplay();
 }
 
 // vim: ft=c
